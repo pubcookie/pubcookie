ff6a15f21b80a8b971bd4c310fd16c571b35b8b7
ivandeex <ivandeex@gmail.com>
2010-11-22 13:37:24 +0000

    substitute for pbc_path.h.in

---
+++ src/nginx/pbc_path.h
@@ -0,0 +1,67 @@
+/* ========================================================================
+ * Copyright 2008 University of Washington
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ========================================================================
+ */
+
+/*
+ * pbc_path.h, The pubcookie path.
+ *
+ * So that we can just #include this file instead of having to 
+ * worry about shell escapes and whatnot.
+ * 
+ * $Id: pbc_path.h.in,v 2.16 2008/05/16 22:09:10 willey Exp $
+ */
+
+
+#ifndef PBC_PATH_H
+#define PBC_PATH_H
+
+#define PBC_PATH (libpbc_config_getstring(p, "pbc_path", "/usr/pubcookie/"))
+/* the login server builds it's key Filenames from the hostname     */
+#if defined (WIN32)
+#  define PBC_KEY_DIR (AddSystemRoot("\\inetsrv\\pubcookie\\keys"))
+#else
+#  define PBC_KEY_DIR (libpbc_config_getstring(p, "keydir", "/usr/pubcookie/keys"))
+#endif
+
+/* where the runtime configuration file lives */
+#define PBC_CONFIG "/usr/pubcookie/config"
+
+/* Where to find templates */
+#define TMPL_FNAME (libpbc_config_getstring(p, "template_root", "/usr/pubcookie/login_templates"))
+
+/* suffix for seperate config files for components */
+#define PBC_SUBCONFIG (libpbc_config_getstring(p, "subconfig_suffix", ".conf"))
+
+/* file to get the list of ok browsers from */
+#define OK_BROWSERS_FILE (libpbc_config_getstring(p, "ok_browsers", "/usr/pubcookie/ok_browsers"))
+
+/* file to get browser information from */
+#define BROWSERS_FILE (libpbc_config_getstring(p, "browsers", "/usr/pubcookie/browsers"))
+
+/* getcred authz file */
+#define GETCRED_AUTHZ (libpbc_config_getstring(p, "getcred_authz_file", "/usr/pubcookie/getcred_authz"))
+
+/* The keyclient.  This is probably better defined elsewhere, but I dunno. */
+
+#define KEYCLIENT (libpbc_config_getstring(p, "keyclient", "/usr/pubcookie/keyclient"))
+
+/* The path to the cert db, if this is using the Netscape SDK */
+
+#define CERT_DB_PATH (libpbc_config_getstring(p, "cert_db_path", "/usr/pubcookie/keys"))
+
+#define SHADOW_PATH (libpbc_config_getstring(p, "shadow_path", "/etc/shadow"))
+
+#endif
