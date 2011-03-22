60aefe898a2a89fa610daaf2eaa84f441fd0c4f1
ivandeex <ivandeex@gmail.com>
2011-03-22 19:49:51 +0000

    automate version extraction from .spec

--- vitki-makedist.sh
+++ vitki-makedist.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-set -x
-VER=3.3.5
-SVNVER=`echo " $Revision$ " | tr -cd 0-9`
-tar -cz -f ../pubcookie-${VER}.r${SVNVER}.tar.gz --exclude .svn --exclude 'nginx*.gz' --exclude `basename $0` *
+#set -x
+SPEC=pubcookie-vitki.spec
+[ ! -r $SPEC ] && echo "$SPEC: cannot open" && exit
+VER=`grep define $SPEC | grep pubver | head -1 | awk '{print $3}'`
+SVNVER=`grep define $SPEC | grep svnid | head -1 | cut -d: -f2 | tr -cd 0-9`
+TARBALL="../pubcookie-${VER}.r${SVNVER}.tar.gz"
+echo version: $VER $SVNVER
+tar -cz -f $TARBALL --exclude .svn --exclude 'nginx*.gz' --exclude `basename $0` *
+echo $TARBALL
